adb76366e26bd379e8b96f87e7a44637b095fd99
Neve-06 <6255234@rts-365.dk>
2025-06-02 10:44:07 +0200

    added tanstack query

--- src/utilities/typicode.js
+++ src/utilities/typicode.js
@@ -1,22 +1,36 @@
 
+import { QueryClient } from "@tanstack/react-query";
+import queryClient from "./queryClient";
 
+// export async function getUsers(){
+//     const response = await fetch("https://jsonplaceholder.typicode.com/user/");
+//     if (!response.ok){
+//         throw new Error("Den er helt gal")
+//     }
+//     setTimeout(() =>{
+//         console.log("Timeout");
 
-export async function getUsers(){
-    const response = await fetch("https://jsonplaceholder.typicode.com/user/");
-    if (!response.ok){
-        throw new Error("Den er helt gal")
-    }
-    setTimeout(() =>{
-        console.log("Timeout");
-        
-    }, 2000)
-    return response.json();
+//     }, 2000)
+//     return response.json();
+// }
+
+export async function getUsers() {
+    return queryClient.fetchQuery({
+        queryKey: ['users'],
+        queryFn: async function() {
+            const response = await fetch("https://jsonplaceholder.typicode.com/users/");
+            if (!response.ok) {
+                throw new Error("Den er helt gal")
+            }
+            return response.json()
+        }
+    })
 }
-export async function getUser({params}){
+export async function getUser({ params }) {
     console.log(params);
-    
+
     const response = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`);
-        if (!response.ok){
+    if (!response.ok) {
         throw new Error("Network response was not ok")
     }
 
